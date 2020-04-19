3b0397f0aa1041fbfef2c58b23f963c361fa4ab1
Idriss Dimson <idriss.dimson@gmail.com>
2020-04-18 23:19:05 -0400

    first commit of final project

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function drawDino() {}
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+}
+
+function draw() {
+  background(200, 200, 200);
+  line(0, height / 2, width, height / 2);
+}
