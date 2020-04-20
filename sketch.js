fe5c14a5ff4e5c3328d6596b14fafa5932e07920
Idriss Dimson <idriss.dimson@gmail.com>
2020-04-20 12:36:01 -0400

    started working on adding the dino to the game

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
+// let dino, dinoRun1, dinoRun2, dinoJump, dinoDead;
+let dino;
 function drawDino() {}
 
+function preload() {
+  dino = loadImage('data/dino0000.png');
+  // dinoRun1 = loadImage('data/dinorun0000.png');
+  // dinoRun2 = loadImage('data/dinorun0001.png');
+  // dinoJump = loadImage('data/dinoJump0000.png');
+  // dinoDead = loadImage('data/dinoDead0000.png');
+}
 function setup() {
   createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
-  background(200, 200, 200);
+  background(247);
   line(0, height / 2, width, height / 2);
+  image(dino, width / 2, height / 2, dino.width * 4, dino.height * 4);
 }
