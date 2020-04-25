ebef207eb03f2bec91b7b8fcd932065545eea85f
Idriss Dimson <idriss.dimson@gmail.com>
2020-04-25 14:18:48 -0400

    added running/jumping animations

--- sketch.js
+++ sketch.js
@@ -1,20 +1,44 @@
-// let dino, dinoRun1, dinoRun2, dinoJump, dinoDead;
-let dino;
-function drawDino() {}
+let dino, platform;
 
-function preload() {
-  dino = loadImage('data/dino0000.png');
-  // dinoRun1 = loadImage('data/dinorun0000.png');
-  // dinoRun2 = loadImage('data/dinorun0001.png');
-  // dinoJump = loadImage('data/dinoJump0000.png');
-  // dinoDead = loadImage('data/dinoDead0000.png');
-}
+const GRAVITY = 1;
+let isRunning = true;
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
+
+  // create the sprites
+
+  // dino
+  dino = createSprite(width / 2, 0, 100, 100);
+  dino.addAnimation('running', 'data/dinorun0000.png', 'data/dinorun0001.png');
+  dino.addAnimation('jumping', 'data/dinoJump0000.png');
+  //platform
+  platform = createSprite(width / 2, height / 2 + 60, width, 5);
+  platform.setCollider('rectangle');
+  platform.shapeColor = color(0, 0, 0);
 }
 
 function draw() {
-  background(247);
-  line(0, height / 2, width, height / 2);
-  image(dino, width / 2, height / 2, dino.width * 4, dino.height * 4);
+  background(250);
+  dino.mirrorX(-1);
+
+  dino.collide(platform, (dino) => {
+    dino.changeAnimation('running');
+    isRunning = true;
+  });
+  dino.velocity.y += GRAVITY;
+
+  dino.debug = mouseIsPressed;
+  platform.debug = mouseIsPressed;
+  // draw the sprites
+  drawSprites();
+}
+
+function keyPressed() {
+  if (keyCode === 32 && isRunning) {
+    dino.changeAnimation('jumping');
+    dino.velocity.y = -10;
+    isRunning = false;
+  }
+
+  return false;
 }
